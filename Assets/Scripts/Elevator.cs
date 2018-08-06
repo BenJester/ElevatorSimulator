a90c4ae97e228e4a8733356d8fee00c582e3a8ff
Ben Yang <ben_yang@foxmail.com>
2018-08-06 17:28:37 +0800

    initial commit

---
+++ Assets/Scripts/Elevator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct ElevatorPos {
+	public int ID;
+	public int Floor;
+
+	public ElevatorPos(int id, int floor) {
+		ID = id;
+		Floor = floor;
+	}
+}
+
+public class Elevator : MonoBehaviour {
+	
+	public ElevatorPos Pos;
+	public float Speed;
+
+
+}
--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -2,15 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class World : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		Debug.Log ("Hello");
+	private static World instance;
+	public static World Instance { 
+		get { 	
+			return instance; 
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+	public List<Elevator> Elevators;
+
+	void Start() {
+		if (World.instance == null) {
+			World.instance = this;
+		} else {
+			Debug.LogError ("cannot have two worlds");
+		}
+
+		Elevators = new List<Elevator> ();
+	}
+
+	public ElevatorPos TouchToElevatorPos(Vector2 pos) {
 		
+		return new ElevatorPos(1,1);
+	}
+
+	void LeftClick() {
+		/*
+		RaycastHit2D hit = Physics2D.Raycast(new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x,Camera.main.ScreenToWorldPoint(Input.mousePosition).y), Vector3.forward, Mathf.Infinity);
+		if (hit){
+			
+		}
+		*/
+		if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began))
+		{
+			
+			/*
+			Ray raycast = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+			RaycastHit raycastHit;
+			if (Physics.Raycast(raycast, out raycastHit))
+			{
+				if (raycastHit.collider.CompareTag("Elevator"))
+				{
+					Debug.Log("Elevator clicked");
+				}
+			}
+			*/
+		}
 	}
 }
